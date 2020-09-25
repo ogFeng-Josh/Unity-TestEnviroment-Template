26c83dbf31db9548497cb91b23a6a39d672b0182
Josh <jmyphotographybooking@gmail.com>
2020-09-24 20:41:23 -0700

    Unity Prototypes for TEST Enviroment

---
+++ prototypesGround/Assets/Template/Scripts/mainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    //For Transition Fade
+    public Animator transition;
+    public float transitionTime = 1f;
+
+
+    public void playgame(){
+    StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void quitgame(){
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    //For Transition Fade
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+}
---
+++ prototypesGround/Assets/Template/Scripts/pauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public string mainMenuName;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Game Quit!");
+        Application.Quit();
+    }
+
+}
